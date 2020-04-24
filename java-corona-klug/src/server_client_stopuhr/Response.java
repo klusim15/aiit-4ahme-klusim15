0041793a0dd7b726d273d7dd81ee3faf4f0bbfd8
klusim15 <simonklug@gmx.at>
2020-04-24 11:05:41 +0200

    Repository aktualisiert

---
+++ java-corona-klug/src/server_client_stopuhr/Response.java
@@ -0,0 +1,51 @@
+package server_client_stopuhr;
+
+/**
+ *
+ * @author Simon Klug
+ */
+public class Response {
+    private boolean master;
+    private long count;
+    private boolean running;
+    private long time;
+
+    public Response(boolean master, long count, boolean running, long time) {
+        this.master = master;
+        this.count = count;
+        this.running = running;
+        this.time = time;
+    }
+
+    public boolean isMaster() {
+        return master;
+    }
+
+    public void setMaster(boolean master) {
+        this.master = master;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
--- java-corona-klug/src/server_client_stopuhr/Server.java
+++ java-corona-klug/src/server_client_stopuhr/Server.java
@@ -1,6 +1,9 @@
 package server_client_stopuhr;
 
+import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -13,32 +16,33 @@ import java.util.List;
 public class Server {
     
     private ServerSocket serverSocket;
-    private final List<ConnectionHandler> handler = new ArrayList<>();
+    private final List<ConnectionHandler> handlers = new ArrayList<>();
     private long timeOffset;
     private long startMillis;
     
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
+        timeOffset = 0;
+        startMillis = System.currentTimeMillis();
         while (true) {
             final Socket clientSocket = serverSocket.accept();
-            ConnectionHandler handler = new ConnectionHandler(clientSocket);
-            Thread thread = new Thread(handler);
-            thread.start();
+            final ConnectionHandler handler = new ConnectionHandler(clientSocket);
+            new Thread(handler).start();
+            handlers.add(handler);
         }
     }
     
     public boolean isTimerRunning() {
-        
+        return true;
     }
     
     public long getTimerMillis (){
-        long epochTime = System.currentTimeMillis();
-        System.out.println(epochTime);
-        return startMillis = 0;
+        long epochTime = System.currentTimeMillis() - startMillis;
+        return epochTime;
     }
     
     public static void main(String[] args) {
-        new Server();
+        Server server = new Server();
     }
 }
 
@@ -57,17 +61,28 @@ class ConnectionHandler implements Runnable {
     }
     
     public boolean isMaster () {
-        
+        return true;
     }
 
     @Override
     public void run() {
-        int i = 0;
-        while (true) {
-          if (Thread.interrupted()) {
-             break;
-          }
-          System.out.println(i++);
-       }
+        try {
+            BufferedReader reader = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            String line;
+            line = reader.readLine();
+            Gson gson = new Gson();
+            gson.toJson(line);
+            Request re = gson.fromJson(line, Request.class);
+            if (re.isMaster()) {
+                for (ConnectionHandler ch : handlers) {
+                    this.master = true;
+                    if (ch != this && ch.isMaster == true) {
+                        this.master = false;
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
