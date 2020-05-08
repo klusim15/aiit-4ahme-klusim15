d2de0ca41833846950730798a7320b9b4f72d236
klusim15 <simonklug@gmx.at>
2020-05-08 11:09:05 +0200

    Repository aktualisiert

--- java-corona-klug/src/server_client_stopuhr/Server.java
+++ java-corona-klug/src/server_client_stopuhr/Server.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public class Server {
     
-    private ServerSocket serverSocket;
+    private final ServerSocket serverSocket;
     private final List<ConnectionHandler> handlers = new ArrayList<>();
     private long timeOffset;
     private long startMillis;
@@ -23,32 +23,38 @@ public class Server {
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         timeOffset = 0;
-        startMillis = System.currentTimeMillis();
         while (true) {
-            final Socket clientSocket = serverSocket.accept();
-            final ConnectionHandler handler = new ConnectionHandler(clientSocket);
-            new Thread(handler).start();
-            handlers.add(handler);
+            final Socket Socket = serverSocket.accept();
+            if (handlers.size < 3) {
+                final ConnectionHandler handler = new ConnectionHandler(clientSocket);
+                new Thread(handler).start();
+                handlers.add(handler);
+            } else {
+                clientSocket.close();
+            }
         }
     }
     
     public boolean isTimerRunning() {
-        return true;
+        return startMillis > 0;
     }
     
     public long getTimerMillis (){
-        long epochTime = System.currentTimeMillis() - startMillis;
-        return epochTime;
+        if (startMillis > 0) {
+            return System.currentTimeMillis() - startMillis + timeOffset;
+        } else {
+            return epochTime;
+        }
     }
     
     public static void main(String[] args) {
-        Server server = new Server();
+        new Server().start(8080);
     }
 }
 
 class ConnectionHandler implements Runnable {
     
-    private Socket socket;
+    private final Socket socket;
     private boolean master;
 
     public ConnectionHandler(Socket socket) {
@@ -57,29 +63,49 @@ class ConnectionHandler implements Runnable {
     
     public boolean isClosed () {
         
-        return true;
+        return socket.isClosed;
     }
     
     public boolean isMaster () {
-        return true;
+        return master;
     }
 
     @Override
     public void run() {
         try {
-            BufferedReader reader = new BufferedReader (new InputStreamReader(socket.getInputStream()));
-            String line;
-            line = reader.readLine();
-            Gson gson = new Gson();
-            gson.toJson(line);
-            Request re = gson.fromJson(line, Request.class);
-            if (re.isMaster()) {
+            while (true) {
+                final BufferedReader reader = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+                final String line = reader.Line();
+                final Gson gson = new Gson();
+                final Request re = gson.fromJson(line, Request.class);
+
+                if (re.isMaster()) {
+                boolean setMasterTrue = true;
                 for (ConnectionHandler ch : handlers) {
-                    this.master = true;
-                    if (ch != this && ch.isMaster == true) {
-                        this.master = false;
+                    if (!ch.equals(this) && ch.isMaster == true) {
+                        setMasterTrue = false;
+                        break;
                     }
                 }
+                master = setMasterTrue;
+            }
+
+            if (re.isStart()) {
+                startMillis = System.currentTimeMillis();
+            }
+            if (re.isClear()) {
+                if (isTimerRunning) {
+                    startMillis = System.currentTimeMillis;
+                }
+                timeOffset = 0;
+            }
+            if (re.isStop()) {
+                timeOffset = getTimeMillis();
+                startMillis = 0;
+            }
+            if (re.isEnd()) {
+                socket.close();
+                handlers.remove(this);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
